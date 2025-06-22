12921dcf1255dd75c26cd3bcdbc34edc87f22cea
Abhishek Anand <abhishekanand7091@gmail.com>
2025-06-22 22:32:04 +0530

    resolve valid email issue

--- src/Views/Components/AddressForm/AddressForm.tsx
+++ src/Views/Components/AddressForm/AddressForm.tsx
@@ -52,7 +52,7 @@ export default function AddressForm({
 
       emailAddress: Yup.string()
         .required(VALIDATION.REQUIRED)
-        .matches(VALIDATION.Email_REGEX),
+        .matches(VALIDATION.Email_REGEX, VALIDATION.EMAIL_INVALID),
     }),
 
     onSubmit: async (values, { resetForm }) => {
